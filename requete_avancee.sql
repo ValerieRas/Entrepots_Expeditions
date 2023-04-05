8fa79241181ab8d8e5d65c7fb9f979cbc6c87c41
valerieRas <rasolovalerie@gmail.com>
2023-04-05 16:59:30 +0200

    tout ok sauf les jours ouvrables

--- requete_avancee.sql
+++ requete_avancee.sql
@@ -106,6 +106,7 @@ GROUP BY nom_entrepot;
 
 --Affichez les expéditions qui ont été livrées dans un délai de moins de 5 jours ouvrables
 
+---marche mais compte le weekend---
 SELECT 
 	*
 FROM
@@ -114,7 +115,7 @@ WHERE
 	statut='arriver' 
 	AND DATEPART(weekday, date_expedition) NOT IN (7,6) AND DATEDIFF(day, date_expedition, GETDATE())<=5;
 
-
+---marche mais compte le weekend---
 SELECT *
 FROM (
 		SELECT *
@@ -128,8 +129,127 @@ WHERE
 	AND DATEDIFF(day, JOUR_OUVRABLE.date_expedition, DATEPART(weekday,GETDATE()))<=5;	
 
 
+---marche mais compte le weekend---
+SELECT *
+FROM expeditions
+WHERE DATEDIFF(DAY, date_expedition, GETDATE()) <= 5
+AND DATEDIFF(WEEK, date_expedition, GETDATE()) * 2 +
+    CASE WHEN DATEPART(WEEKDAY, date_expedition) = 7 THEN 1 ELSE 0 END +
+    CASE WHEN DATEPART(WEEKDAY, GETDATE()) = 1 THEN 1 ELSE 0 END <= 5
+AND statut = 'arriver';
 
---
+
+---marche mais compte le weekend et azu dela de 5jours---
+CREATE FUNCTION fn_remove_weekend (@inputDate DATE)
+RETURNS DATE
+AS
+BEGIN
+    DECLARE @outputDate DATE = @inputDate
+
+    -- Retire le samedi
+    WHILE DATEPART(WEEKDAY, @outputDate) = 7
+        SET @outputDate = DATEADD(DAY, 1, @outputDate)
+
+    -- Retire le dimanche
+    WHILE DATEPART(WEEKDAY, @outputDate) = 1
+        SET @outputDate = DATEADD(DAY, 1, @outputDate)
+
+    RETURN @outputDate
+END
+
+SELECT *
+FROM expeditions
+WHERE DATEDIFF(day, dbo.fn_remove_weekend(GETDATE()), date_expedition) <= 5
+  AND DATEDIFF(day, dbo.fn_remove_weekend(date_expedition), date_expedition) <= 5
+  AND statut = 'arriver'
+
+
+---marche mais compte le weekend---
+
+CREATE FUNCTION dbo.fn_rm_weekend (@inputDate DATE)
+RETURNS DATE
+AS
+BEGIN
+    DECLARE @outputDate DATE = @inputDate
+
+    -- Retire le samedi
+    WHILE DATEPART(WEEKDAY, @outputDate) = 7
+        SET @outputDate = DATEADD(DAY, 1, @outputDate)
+
+    -- Retire le dimanche
+    WHILE DATEPART(WEEKDAY, @outputDate) = 1
+        SET @outputDate = DATEADD(DAY, 1, @outputDate)
+
+    RETURN @outputDate
+END
+
+
+SELECT *
+FROM expeditions
+WHERE DATEDIFF(d, dbo.fn_remove_weekend(date_expedition), GETDATE()) <= 5
+  AND DATEDIFF(d, dbo.fn_remove_weekend(GETDATE()), dbo.fn_remove_weekend(date_expedition)) <= 5 
+  AND statut = 'arriver';
+
+CREATE FUNCTION expeditions_inf_5_jours_ouvrables()
+RETURNS TABLE
+AS
+RETURN (
+    SELECT *
+    FROM expeditions
+    WHERE statut = 'arriver'
+      AND DATEDIFF(WEEKDAY, date_expedition, GETDATE()) < 5
+);
+
+SELECT * FROM expeditions_inf_5_jours_ouvrables();
+
+
+----
+
+SELECT *
+FROM expeditions
+WHERE date_expedition >= DATEADD(dd, -4, GETDATE())
+AND DATEDIFF(dd, date_expedition, GETDATE()) - 
+    (DATEDIFF(wk, date_expedition, GETDATE()) * 2) -
+    CASE WHEN DATENAME(dw, date_expedition) = 'Sunday' THEN 1 ELSE 0 END -
+    CASE WHEN DATENAME(dw, GETDATE()) = 'Saturday' THEN 1 ELSE 0 END < 5
+
+----
+CREATE FUNCTION fn_remove_week (@inputDate DATETIME)
+RETURNS DATETIME
+AS
+BEGIN
+    DECLARE @outputDate DATETIME = @inputDate
+
+    -- Retire le samedi
+    IF DATEPART(WEEKDAY, @outputDate) = 7
+        SET @outputDate = DATEADD(DAY, 2, @outputDate)
+
+    -- Retire le dimanche
+    IF DATEPART(WEEKDAY, @outputDate) = 1
+        SET @outputDate = DATEADD(DAY, 1, @outputDate)
+
+    RETURN @outputDate
+END
+
+SELECT *
+FROM expeditions
+WHERE DATEDIFF(dd, date_expedition, dbo.fn_remove_week(GETDATE())) <= 5 
+  AND DATEDIFF(dd, dbo.fn_remove_week(GETDATE()), GETDATE()) <= 5
+  AND statut = 'arriver'
+
+
+
+  --- marche mais prends en compte le weekend
+SELECT *
+FROM expeditions
+WHERE date_expedition >= DATEADD(day, -5, GETDATE())
+AND DATEDIFF(day, date_expedition, GETDATE()) - 
+    (DATEDIFF(week, date_expedition, GETDATE()) * 2) -
+    CASE WHEN DATENAME(weekday, date_expedition) = 'Sunday' THEN 1 ELSE 0 END -
+    CASE WHEN DATENAME(weekday, GETDATE()) = 'Saturday' THEN 1 ELSE 0 END < 5
+
+
+
 SELECT * FROM entrepots;
 SELECT * FROM EXPEDITIONS;
 
--- requete_complexe.sql
+++ requete_complexe.sql
@@ -16,6 +16,9 @@ VALUES
 
 INSERT INTO expeditions (date_expedition, id_entrepot_source, id_entrepot_destination, poids, statut)
 VALUES
+	('2023-04-01',8,5,896,'en transit'),
+	('2023-04-04',3,5,89,'en transit'),
+	('2023-03-28',5,8,540,'arriver'),
     ('2023-03-20', 8, 4, 1200, 'en transit'),
     ('2023-03-21', 3, 7, 180.6, 'en transit'),
     ('2023-03-28',4,6,40,'arriver');
@@ -33,20 +36,34 @@ WHERE entrs.continent='europe' AND entrd.continent='Asie';
 
 
 --Affichez les entrepôts qui ont envoyé des expéditions à destination d'un entrepôt situé dans le même pays.
-SELECT 
-	*
-FROM
-	expeditions expe
-INNER JOIN entrepots entrs
-	ON expe.id_entrepot_source=entrs.id
-INNER JOIN entrepots entrd
-	ON expe.id_entrepot_destination=entrd.id
-WHERE entrs.pays=entrd.pays;
+
+SELECT DISTINCT *
+FROM expeditions expe
+INNER JOIN entrepots entre1 ON entre1.id = expe.id_entrepot_source
+INNER JOIN entrepots entre2 ON entre2.id = expe.id_entrepot_destination
+WHERE entre1.pays = entre2.pays;
+
+
 
 --Affichez les entrepôts qui ont envoyé des expéditions à destination d'un entrepôt situé dans un pays différent.
 
+SELECT DISTINCT *
+FROM expeditions expe
+INNER JOIN entrepots entre1 ON entre1.id = expe.id_entrepot_source
+INNER JOIN entrepots entre2 ON entre2.id = expe.id_entrepot_destination
+WHERE entre1.pays != entre2.pays;
+
+
 --Affichez les expéditions en transit qui ont été initiées par un entrepôt situé dans un pays dont le nom commence par la lettre "F" et qui pèsent plus de 500 kg.
 
+UPDATE expeditions SET poids=570 WHERE id=1;
+
+SELECT *
+FROM expeditions e
+JOIN entrepots ent1 ON e.id_entrepot_source = ent1.id
+WHERE e.statut = 'en transit'
+AND ent1.pays LIKE 'F%'
+AND e.poids > 500;
 
 --Affichez le nombre total d'expéditions pour chaque combinaison de pays d'origine et de destination.
 
@@ -62,12 +79,34 @@ GROUP BY e1.pays, e2.pays;
 
 --Affichez les entrepôts qui ont envoyé des expéditions au cours des 30 derniers jours et dont le poids total des expéditions est supérieur à 1000 kg.
 
+SELECT 
+	ent1.nom_entrepot,
+	sum(poids)
+FROM expeditions e
+JOIN entrepots ent1 ON e.id_entrepot_source = ent1.id
+WHERE
+	DATEDIFF(day, date_expedition, GETDATE())<=30
+GROUP BY 
+	ent1.nom_entrepot
+HAVING
+	sum(e.poids) > 1000;
 
 --Affichez les expéditions qui ont été livrées avec un retard de plus de 2 jours ouvrables.
 
 
 --Affichez le nombre total d'expéditions pour chaque jour du mois en cours, trié par ordre décroissant.
 
+SELECT 
+	DAY(date_expedition) as jour,
+	count(*) as nombre_expedition
+FROM
+	expeditions
+WHERE 
+	month(date_expedition)=month(getdate())
+GROUP BY DAY(date_expedition)
+ORDER BY nombre_expedition DESC;
+
 
 SELECT * FROM entrepots;
 select * FROM expeditions;
+
